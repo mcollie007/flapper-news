75fe0af626c25a44e5eb799698e9808f5228f00a
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-02-24 16:55:43 -0500

    generated models, controllers, updated db
    -added active record for posts and comments
    -added references between posts and comments models
    -added controllers for posts and comments
    -added routes to link up votes to posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root to: 'application#angular'
+
+  resources :posts, only: [:create, :index, :show] do
+    resources :comments, only: [:show, :create] do
+      member do
+        put '/upvote' => 'comments#upvote'
+      end
+    end
+
+    member do
+      put '/upvote' => 'posts#upvote'
+    end
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
